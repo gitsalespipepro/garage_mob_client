02e960f89dd333ef24bbd9bc0926ebe4c35f12d8
Rezo Joglidze <rezojoglidze7@gmail.com>
2025-11-11 06:47:47 +0400

    GN-2155: set tab bar appearance

--- GarageMobClient/App/GarageMobClientApp.swift
+++ GarageMobClient/App/GarageMobClientApp.swift
@@ -11,6 +11,15 @@ import SwiftUI
 struct GarageMobClientApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
+    init() {
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = UIColor.white
+        
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
+    
     var body: some Scene {
         WindowGroup {
             TabBarView()
